8f39accfcd24ccf44701231c7386b0e94265112a
angielxx <angielxx94@gmail.com>
2022-09-11 11:20:35 +0900

    work in progress 220911

--- accounts/models.py
+++ accounts/models.py
@@ -1,25 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
+    def create_user(self, email, password=None):
         if not email:
             raise ValueError("Users must have an email address")
 
         user = self.model(
             email=self.normalize_email(email),
-            name=name,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, name, password=None):
+    def create_superuser(self, email, password=None):
         user = self.create_user(
             email,
             password=password,
-            name=name
         )
         user.is_admin = True
         user.save(using=self._db)
